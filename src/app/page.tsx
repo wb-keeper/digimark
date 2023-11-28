3e1f0dd2624962f7ef723cf274f6296c2448f798
wb-keeper <wb-keeper@mail.ru>
2023-11-28 09:01:03 +0300

    some minor changes on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
 //24 25
 export default function Home() {
   return (
     <MaxWidthWrapper className="">
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <div>hello</div>
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality
+          <span className="text-blue-600"> digital assets</span>.
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcome to DigiMark. Every asset on our platform is verified by our
+          team to ensure our highest quality standards.
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="/products">Browse Trending</Link>
+        </div>
       </div>
     </MaxWidthWrapper>
   );
