d7704b71cd28cb0e0f3f9007ac89667bf7967ae1
wb-keeper <wb-keeper@mail.ru>
2023-11-30 08:47:55 +0300

    Working on NavItems

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { FC } from "react";
+import { Button } from "@/components/ui/button";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+type Category = (typeof PRODUCT_CATEGORIES)[number];
+interface NavItemProps {
+  category: Category;
+  handleOpen: () => void;
+  isOpen: boolean;
+  isAnyOpen: boolean;
+}
+
+const NavItem: FC<NavItemProps> = ({
+  isAnyOpen,
+  isOpen,
+  category,
+  handleOpen,
+}) => {
+  return (
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? "secondary" : "ghost"}
+        >
+          {category.label}
+          <ChevronDown
+            className={cn("h-4 w-4 transition-all text-muted-foreground", {
+              "-rotate-180": isOpen,
+            })}
+          />
+        </Button>
+      </div>
+      {isOpen ? (
+        <div
+          className={cn(
+            "absolute inset-x-0 top-full text-sm text-muted-foreground",
+            {
+              "animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen,
+            },
+          )}
+        >
+          <div
+            className="absolute inset-0 top-1/2 bg-white shadow"
+            aria-hidden="true"
+          >
+            <div className="relative bg-white">
+              <div className="mx-auto max-w-7xl px-8">
+                <div className="gred grid-cols-4 gap-x-8 gap-y-10 py-16">
+                  <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+                    {category.featured.map((item, i) => (
+                      <div
+                        key={item.name}
+                        className="group relative text-base sm:text-sm"
+                      >
+                        <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                          <Image
+                            src={item.imageSrc}
+                            alt="product category image"
+                            className="object-cover"
+                            fill
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default NavItem;
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,12 +1,37 @@
 "use client";
 
 import { FC, useState } from "react";
+import { PRODUCT_CATEGORIES } from "@/config";
+import NavItem from "@/components/NavItem";
 
 interface NavItemsProps {}
 
 const NavItems: FC<NavItemsProps> = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
-  return <div className="flex gap-4 h-full">menu</div>;
+  const isAnyOpen = activeIndex !== null;
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+        const isOpen = i === activeIndex;
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          ></NavItem>
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
