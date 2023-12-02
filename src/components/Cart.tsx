bcbb698452ad9123747b2bf6951e2886f3ec5157
wb-keeper <wb-keeper@mail.ru>
2023-12-02 19:01:09 +0300

    Finished Cart component & started on Express installing

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,17 +3,23 @@ import { FC } from "react";
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { ShoppingCart } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
 
 interface CartProps {}
 
 const Cart: FC<CartProps> = () => {
-  const itemCount = 1;
+  const itemCount = 0;
+  const fee = 1;
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -41,13 +47,53 @@ const Cart: FC<CartProps> = () => {
                 </div>
                 <div className="flex">
                   <span className="flex-1">Transaction Fee</span>
-                  <span>1</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span>{formatPrice(fee)}</span>
                 </div>
               </div>
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({
+                      className: "w-full",
+                    })}
+                  >
+                    Continue to Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
             </div>
           </>
         ) : (
-          <div>123</div>
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+              aria-hidden="true"
+            >
+              <Image
+                src="/hippo-empty-cart.png"
+                fill
+                alt="empty shopping cart"
+              />
+            </div>
+            <div className="text-lx font-semibold">Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link
+                href="/products"
+                className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "text-sm text-muted-foreground",
+                })}
+              >
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
+          </div>
         )}
       </SheetContent>
     </Sheet>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,4 +14,10 @@ export function formatPrice(
 ) {
   const { currency = "USD", notation = "compact" } = options;
   const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
 }
