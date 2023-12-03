c41db1d0c9a4b1303ca39faf634830b1a635fca6
wb-keeper <wb-keeper@mail.ru>
2023-12-03 19:17:27 +0300

    Inslalled express payload packages + created config files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
-//1 46
+//1 55
 const perks = [
   {
     name: "Instant Delivery",
